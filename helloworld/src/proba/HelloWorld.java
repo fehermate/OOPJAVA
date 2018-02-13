4072d3aaa8a215f957ed37270562930bb4178942
fehermate <fehermate0514@gmail.com>
2018-02-13 17:50:46 +0100

    Initial commit

---
+++ helloworld/src/proba/HelloWorld.java
@@ -0,0 +1,11 @@
+package proba;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		int valt=10;
+		
+		System.out.println("Hello World"+"ddddd"+' '+valt);
+		System.out.println(valt);
+		
+	}
+}
