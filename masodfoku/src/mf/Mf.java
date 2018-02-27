256420df3f9dc320f746730a896d1e9ad1e66092
fehermate <fehermate0514@gmail.com>
2018-02-27 17:39:41 +0100

    New

--- masodfoku/src/mf/Mf.java
+++ masodfoku/src/mf/Mf.java
@@ -1,23 +1,42 @@
 package mf;
 
+import java.util.Scanner;
+
 public class Mf {
 
 //	
 	public static void main(String[] args) {
-	int a = 5;
-	int b = 20;
-	int c = 2;
+	int egyutt[] = new int[3];
 	double discriminant = 0;
 	double x1,x2 = 0;
+	Scanner sc = new Scanner(System.in);
+	int a = 0;
+	int b = 0;
+	int c = 0;
 	
-	discriminant = Math.sqrt((b * b) - 4 * a * c);
+	System.out.println("Kérem az 1. együtthatót: ");
+	egyutt[0] = sc.nextInt();
 	
-	if (Double.isNaN(discriminant)) {
+	System.out.println("Kérem a 2. együtthatót: ");
+	egyutt[1] = sc.nextInt();
+	
+	System.out.println("Kérem a 3. együtthatót: ");
+	egyutt[2] = sc.nextInt();
+	
+	sc.close();
+	
+	a = egyutt[0];
+	b = egyutt[1];
+	c = egyutt[2];
+	
+	discriminant = (b * b) - 4 * a * c;
+	
+	if ((discriminant)<0) {
 		System.out.println("Nincs valós gyöke.");
 	}
 	else {
-		x1 = (b * (-1) + discriminant) / (2 * a);
-		x2 = (b * (-1) - discriminant) / (2 * a);
+		x1 = (b * (-1) + Math.sqrt(discriminant)) / (2 * a);
+		x2 = (b * (-1) - Math.sqrt(discriminant)) / (2 * a);
 		System.out.println("A megoldások: x1:" + x1 + " x2:" + x2);
 	}
 	
