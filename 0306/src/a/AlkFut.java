8e11e8486596dad4ea103263959e6933792727c6
fehermate <fehermate0514@gmail.com>
2018-03-06 17:17:06 +0100

    Március 06

---
+++ 0306/src/a/AlkFut.java
@@ -0,0 +1,30 @@
+package a;
+
+public class AlkFut {
+
+	public static void main(String[] args) {
+		Alkalmazott alk1 = new Alkalmazott();
+		alk1.setName("Tomi");
+		alk1.setPayment(1);
+		
+		Alkalmazott alk2 = new Alkalmazott();
+		alk2.setName("Tomi2");
+		alk2.setPayment(80);
+		
+		System.out.println(alk1.szovegVissza());
+		alk1.fizNov(10);
+		System.out.println(alk1.szovegVissza());
+		
+		boolean isTrue = (alk1.fizHatar(10, 20));
+		if(isTrue)
+			System.out.println("A megadott határok közé esik a fizetése.");
+		else System.out.println("Nem esik a megadott határok közé esik a fizetése.");
+		
+		System.out.println("Az adó értéke: " + alk1.getAdo());
+		
+		if((alk1.nagyobbE(alk2)))
+			System.out.println("Az alk1 fizetése nagyobb.");
+		else System.out.println("Az alk2 fizetése nagyobb.");
+	}
+
+}
